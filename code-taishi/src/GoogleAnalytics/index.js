bd1ab1540db7dda1e8c96546c817fca488359080
cheshari-pearl <chesharipearl@gmail.com>
2024-07-16 16:15:16 +0300

    google analytics

--- code-taishi/src/GoogleAnalytics/index.js
+++ code-taishi/src/GoogleAnalytics/index.js
@@ -1,5 +1,5 @@
 import ReactGA from 'react-ga';
 const initializeGoogle = () => {
-    ReactGA.initialize('G-XG3KW3VLD9'); // Replace with your Measurement ID
+    ReactGA.initialize('G-WDXHL38WNL'); // Replace with your Measurement ID
   };
   export default initializeGoogle;
