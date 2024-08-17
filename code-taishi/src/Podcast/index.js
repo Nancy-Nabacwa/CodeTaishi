db3792ce2d8490a494573cdf59708aa07ce794ea
Lavender <anyangolavenderr@gmail.com>
2024-08-17 09:35:56 +0300

    podcasts

--- code-taishi/src/Podcast/index.js
+++ code-taishi/src/Podcast/index.js
@@ -1,35 +1,40 @@
 import podcast from '../Assets/podcast.png';
-import microphone from '../Assets/microphone.jpg'
+import microphone from '../Assets/microphone.jpg';
 import './index.css';
+import podcastAudio from '../Assets/UnchainedHeartsEP1.mp3';
 import Navbar from "../Navbar";
 
 
-
-
-
-
-const Podcast = () =>{
-
-    return(
-       <>
-       <Navbar/>
-        <section className="banner" style={{ backgroundImage: `url(${podcast})` }}>
+const Podcast = function () {
+  return (
+    <>
+     <Navbar/> 
+      <section className="banner" style={{ backgroundImage: `url(${podcast})` }}>
         <div className="image-container">
           <div className="overlay">
             <div className="content">
-              <h3 className="body-text">CodeTaishi Podcasts</h3>
+              <h3 className="body-text">Taishi Podcasts</h3>
               <div className="coming-soon">
                 <img src={microphone} alt="Coming Soon" />
-                <h3 className="body-text">Coming Soon</h3>
+                <div>
+                <div className="podcast-container">
+      <div className="podcast-player">
+      
+        <audio controls>
+          <source src={podcastAudio} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    </div>
+                  <button className="get-started-btn">All Episodes</button>
+                </div>
               </div>
-            
-              {/* <div id='btn'><button className="get-started-btn">All Episodes</button></div> */}
             </div>
           </div>
         </div>
       </section>
-       </>
-    )
+    </>
+  );
 }
 
 export default Podcast;
